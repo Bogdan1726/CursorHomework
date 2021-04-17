9f1802f09d7a007cd8474292ac918eee192b5b16
Bohdan Rozhniatovskiy <bogdan24ro@gmail.com>
2021-04-17 12:15:57 +0300

    finished hw14

--- HW14/task1.py
+++ HW14/task1.py
@@ -11,15 +11,15 @@ from concurrent.futures import ThreadPoolExecutor
 
 def find_by_patter(filename, pattern):
     line_container = set()
-    with open(filename) as file:
-        for el in file:
-            if pattern in el:
-                line_container.add(el)
+    with open(filename) as f:
+        for line in f:
+            if pattern in line:
+                line_container.add(line)
     return line_container
 
 
 def find_all_by_pattern(directory_path, pattern):
-    files = glob.glob(f'{directory_path}/*.py', recursive=True)
+    files = glob.glob(f'{directory_path}**/*.py', recursive=True)
     container = set()
     with ThreadPoolExecutor() as pool:
         result = pool.map(find_by_patter, files, pattern)
@@ -28,10 +28,12 @@ def find_all_by_pattern(directory_path, pattern):
     return container
 
 
-start = time.time()
-search_by_patter = find_all_by_pattern('.', pattern='f')
-end = time.time() - start
-print(f'Search time in {end} seconds')
+if __name__ == "__main__":
+    start = time.time()
+    search_by_patter = find_all_by_pattern('/home/bogdan/PycharmProjects/pythonProject/CursorHomework/HW10',
+                                           pattern='import')
+    end = time.time() - start
+    print(f'Search time in {end} seconds')
 
-for line in search_by_patter:
-    print(line)
+    for line in search_by_patter:
+        print(line)
